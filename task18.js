fb194ef2795aed083745b4e9d9c791e365dc5abd
Ivan Bezrukov <ivan906730@gmail.com>
2024-01-20 13:39:58 +0300

    Add task18

---
+++ task18.js
@@ -0,0 +1,39 @@
+const initialArray1 = [
+	{ "name": "Viktor", "age": 24 },
+	{ "name": "Abiba", "age": 35 },
+	{ "name": "Viktor", "age": 35 },
+	{ "name": "Abiba", "age": 35 },
+];
+
+const initialArray2 = [
+	{ "name": "Viktor", "age": 24 },
+	{ "name": "Aboba", "age": 16 },
+	{ "name": "Abiba", "age": 35 },
+];
+
+const result = unique(initialArray1, initialArray2);
+
+console.log(result);
+
+function unique(arr1, arr2) {
+  const newArr = [];
+  arr1 = arr1.filter(value => {
+    return typeof value === 'object';
+  });
+  arr2 = arr2.filter(value => {
+    return typeof value === 'object';
+  });
+  arr1.forEach(value => {
+    for (let obj of arr2) {
+      if (value.name === obj.name && value.age === obj.age) {
+        for (let obj of newArr) {
+          if (value.name === obj.name && value.age === obj.age) {
+            return;
+          }
+        }
+        newArr.push(value);
+      }
+    }
+  });
+  return newArr;
+}
