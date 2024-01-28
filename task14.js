ffa8c2582b30c342f3f327108e7248f32c8d6e7f
Ivan Bezrukov <ivan906730@gmail.com>
2024-01-28 22:58:17 +0300

    Fix after review

--- task14.js
+++ task14.js
@@ -1,16 +1,16 @@
 const initialArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const initialArray2 = [1, 3, 4, 5, 7, 8, 9, 10, 20, 30, 40];
 
-const result = compare(initialArray1, initialArray2);
-
-console.log(result);
-
 function compare(arr1, arr2) {
   let newArr = [];
   arr1.forEach(value => {
-    if (typeof value === 'number' && !arr2.includes(value)) {
+    if (!arr2.includes(value)) {
       newArr.push(value);
     }
   });
   return newArr;
 }
+
+const result = compare(initialArray1, initialArray2);
+
+console.log(result);
